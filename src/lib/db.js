2965364e98dec95e16ce21e4ce4911b60ed17d15
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-04-11 20:28:57 +0200

    FEAT: Add a config system

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,23 +1,13 @@
 /* @flow */
 'use strict';
 
-var knexModule = require('knex');
-var databaseModels = require('../model/database');
+import config from 'config';
+import knexModule from 'knex';
+import databaseModels from '../model/database';
 
-console.log('Connection to db..');
+let knex = knexModule(config.get('db'));
 
-var knex = knexModule({
-    client: 'mysql',
-        connection: {
-            host: '192.168.56.101',
-            user: 'episodehunter',
-            password: 'episodehunter',
-            database: 'episodehunter'
-        },
-        debug: true
-});
-
-module.exports = {
+export default {
     q: knex,
     model: databaseModels
 };
