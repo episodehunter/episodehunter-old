5893f8693b94e697a707295d8d5c319f4126c44e
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-04-11 19:30:32 +0200

    FEAT/Add-databas-support: Instantiate Knex

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,38 +1,23 @@
 /* @flow */
 'use strict';
 
+var knexModule = require('knex');
+var databaseModels = require('../model/database');
+
 console.log('Connection to db..');
 
-var validUsers = [
-    {
-        id: 1,
-        username: 'tjoskar'
-    }
-];
-
-function query() : Promise {
-    console.log('Geting data');
-    return new Promise(resolve => {
-        setTimeout(_ => resolve({
-            'id': 5,
-            'title': 'Best film ever'
-        }), 0);
-    });
-}
-
-function getUser(id: number) : Promise {
-    return new Promise((resolve, reject) => {
-        validUsers.some(user => {
-            if (user.id === id) {
-                resolve(user);
-                return true;
-            }
-        });
-        reject('No user found :(');
-    });
-}
+var knex = knexModule({
+    client: 'mysql',
+        connection: {
+            host: '192.168.56.101',
+            user: 'episodehunter',
+            password: 'episodehunter',
+            database: 'episodehunter'
+        },
+        debug: true
+});
 
 module.exports = {
-    query: query,
-    getUser: getUser
+    q: knex,
+    model: databaseModels
 };
