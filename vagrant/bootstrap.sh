171eaca649a6f40081b8b9a3acd515a51a0e79b5
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-01 18:03:36 +0200

    Add a vagrant file
    This will automatically install mysql and map 3306 to 33060 on your
    local machine

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+
+if [ -e /.installed ]; then
+  echo 'Already installed.'
+
+else
+  echo ''
+  echo 'INSTALLING'
+  echo '----------'
+
+  # Variables
+  DBNAME=episodehunter
+  DBUSER=episodehunter
+  DBPASSWD=episodehunter
+
+  # Update app-get
+  apt-get update
+
+  echo "\n--- Install base packages ---\n"
+  apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
+
+  echo "\n--- Install MySQL specific packages and settings ---\n"
+  echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+  echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+
+  apt-get -y install mysql-server-5.6 > /dev/null 2>&1
+
+  echo "\n--- Setting up our MySQL user and db ---\n"
+  mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+  mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
+
+  echo "\n--- Updating mysql configs in /etc/mysql/my.cnf ---\n"
+  sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+  echo "\n--- Restarting mysql ---\n"
+  service mysql restart
+
+  # Make sure that vagrant doesn't re-install everything
+  touch /.installed
+fi
