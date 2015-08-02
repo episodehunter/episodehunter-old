a097a0342990cf50d79c10280a272b73bd3efbe7
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-02 19:26:19 +0200

    Add boom and used that when it goes south

--- src/sections/auth/auth-controller.ts
+++ src/sections/auth/auth-controller.ts
@@ -1,6 +1,7 @@
 'use strict';
 
-import Hapi = require('hapi');
+import {Request, IReply} from 'hapi';
+import {forbidden} from 'boom';
 import {autoInject} from 'autoinject';
 import {AuthService} from './auth-service';
 
@@ -9,17 +10,12 @@ class AuthController {
 
     constructor(public service: AuthService) {}
 
-    createToken(request: Hapi.Request, reply: Hapi.IReply): void {
+    createToken(request: Request, reply: IReply): void {
         let username = request.payload['username'];
         let password = request.payload['password'];
         this.service.generateToken(username, password)
             .then(token => reply({token}))
-            .catch(error => {
-                reply({
-                    id: 'forbidden',
-                    reason: 'Forbidden: Invalid username or password'
-                });
-            });
+            .catch(() => reply(forbidden('Invalid username or password')));
     }
 
 }
