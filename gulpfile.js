d79fc22a00dc462590fb5160f2de44e1dd3d24f8
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-09 19:47:51 +0200

    Add a gulp file for simpler development

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var nodemon = require('gulp-nodemon');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('build:typescript', function() {
+    return gulp.src(['./typings/tsd.d.ts', 'src/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('serve', ['build:typescript'], function() {
+    nodemon({
+        script: 'dest/server.js',
+        env: { 'NODE_ENV': 'development' },
+        ext: 'ts',
+        tasks: ['build'],
+        nodeArgs: ['--debug']
+    })
+    .on('restart', function() {
+        console.log('restarted!');
+    });
+});
+
+
+gulp.task('build', ['build:typescript']);
+gulp.task('default', ['build']);
