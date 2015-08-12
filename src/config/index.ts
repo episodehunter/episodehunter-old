2c0d435d45c97a17632661c9b8ce9b35eadb0803
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-12 21:28:05 +0200

    Add a test config file
    Add a config files for tests so we can tweak the system after our needs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,18 @@
 import {configInterface} from './config-interface';
 import {config as defaultConfig} from './default';
 import {config as localConfig} from './local';
+import {config as testConfig} from './test';
 import {config as productionConfig} from './production';
 import {defaultsDeep} from 'lodash';
 
-const production = false;
-const config: configInterface = buildConfig(production);
+const env = process.env.NODE_ENV;
+const config: configInterface = buildConfig(env);
 
-function buildConfig(production) : configInterface {
-    if (production) {
+function buildConfig(env) : configInterface {
+    if (env === 'production') {
         return defaultsDeep<Object, configInterface>(productionConfig, defaultConfig);
+    } else if (env === 'test') {
+        return defaultsDeep<Object, configInterface>(testConfig, defaultConfig);
     } else {
         return defaultsDeep<Object, configInterface>(localConfig, defaultConfig);
     }
