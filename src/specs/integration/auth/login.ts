bc3c4dacc494d5839dfb034f9428ca1062f61577
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-23 17:02:25 +0200

    Add test cases for login

--- src/specs/integration/auth/login.ts
+++ src/specs/integration/auth/login.ts
@@ -43,9 +43,55 @@ describe('Auth', () => {
             return server.injectThen(options)
                 .then(response => {
                     let result = response.result;
-
                     assert.equal(response.statusCode, 200);
                     assert.closeTo(result['token'].length, 150, 50);
+                });
+        });
+
+        it(`Should get forbidden response if trying to login with invalid password`, () => {
+
+            let options: any = {
+                method: 'POST',
+                url: '/auth/create-token',
+                payload: {
+                    username: 'john_snow',
+                    password: 'wrong-password'
+                }
+            };
+
+            tracker.on('query', query => {
+                query.response({
+                    id: 2,
+                    username: 'john_snow',
+                    password: '$2a$10$EgZ6kJhLbgQxDqSqrIUXteRQtzKtxSjldQ4hcTuSBiRYTobaIpOo.'
+                });
+            });
+
+            return server.injectThen(options)
+                .then(response => {
+                    let result = response.result;
+                    assert.equal(response.statusCode, 403);
+                });
+        });
+
+        it(`Should get forbidden response if trying to login with non-existing user`, () => {
+
+            let options: any = {
+                method: 'POST',
+                url: '/auth/create-token',
+                payload: {
+                    username: 'john_snow',
+                    password: 'password'
+                }
+            };
+
+            tracker.on('query', query => {
+                query.response(undefined);
+            });
+
+            return server.injectThen(options)
+                .then(response => {
+                    assert.equal(response.statusCode, 403);
                 });
         });
 
