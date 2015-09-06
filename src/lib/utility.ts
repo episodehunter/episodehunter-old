9154ab6c120625165aa60d2357f9fabe0b828fbe
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-09-06 11:58:44 +0200

    Add a util function for extracting years

--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -1,4 +1,8 @@
 let now = () => Math.floor(Date.now() / 1000);
 let int = val => parseInt(val, 10);
+let extractYear = (date: string): number => {
+    // Expecting the format to be 'YYYY..'
+    return date ? int(date.substr(0, 4)) : 0;
+};
 
-export {now, int};
+export {now, int, extractYear};
