d1d487851a2622c47a43701d9e4348d72f36af94
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-10-11 17:45:46 +0200

    Add a entry for adding and getting watched movies

--- src/start.ts
+++ src/start.ts
@@ -2,6 +2,7 @@ import {queue, Job} from './lib/queue';
 import {logger} from './lib/logger';
 import {scrobble} from './episodehunter-messages/queue/scrobble';
 import * as episodeController from './watched-episodes/controller';
+import * as movieController from './watched-movies/controller';
 
 
 queue.process(scrobble.sync.watched.show.add, 1, (job: Job, done) => {
@@ -28,6 +29,40 @@ queue.process(scrobble.sync.watched.show.get, 10, (job: Job, done) => {
     }
 
     episodeController.getWatched(job.data.userId)
+        .then(data => done(undefined, data))
+        .catch(error => done(error));
+});
+
+queue.process(scrobble.sync.watched.movie.add, 1, (job: Job, done) => {
+    logger.debug('Getting job', job.data);
+    let data = job.data.movie;
+    let userId = job.data.userId;
+
+    if (!data || !userId) {
+        let error = 'Invalid job data: jobId: ' + job.id;
+        logger.error(error);
+        done(new Error(error));
+        return;
+    }
+
+    movieController.setWatched(data, userId)
+        .then(result => done(undefined, result))
+        .catch(error => done(error));
+});
+
+queue.process(scrobble.sync.watched.movie.get, 10, (job: Job, done) => {
+    logger.debug('Getting job', job.data);
+    let data = job.data.movie;
+    let userId = job.data.userId;
+
+    if (!data || !userId) {
+        let error = 'Invalid job data: jobId: ' + job.id;
+        logger.error(error);
+        done(new Error(error));
+        return;
+    }
+
+    movieController.getWatched(job.data.userId)
         .then(data => done(undefined, data))
         .catch(error => done(error));
 });
---
+++ src/watched-movies/controller.ts
@@ -0,0 +1,38 @@
+import {WatchedMovie} from 'eh-domain/model/scrobble/sync';
+import {movieHandler} from '../episodehunter-messages/queue/movie-handler';
+import {addToQueue} from '../lib/queue';
+import {logger} from '../lib/logger';
+import {MissingMovieError} from '../error/missing-movie.error';
+import {setMovieAsWatched, getWatchedMovies} from './service';
+
+
+function setWatched(watchedMovie: WatchedMovie, userId: number) {
+    return setMovieAsWatched(watchedMovie, userId)
+        .catch(error => {
+            if (error instanceof MissingMovieError) {
+                // The movie is missing
+                // Add it and try agin later
+                logger.debug(error.message);
+                addToQueue(movieHandler.movie.add, watchedMovie.ids, {
+                    attempts: 3,
+                    backoff: {delay: 60000, type: 'fixed'}
+                });
+                return Promise.reject(error.message);
+            } else {
+                // Something bad has happened
+                // There's blood everywhere
+                logger.error(error);
+                return Promise.reject(error);
+            }
+        });
+}
+
+function getWatched(userId: number) {
+    return getWatchedMovies(userId)
+        .catch(error => {
+            logger.error(error);
+            return Promise.reject(error);
+        });
+}
+
+export {setWatched, getWatched};
