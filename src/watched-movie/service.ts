31487184335d3720b0d5587c0fb7cabdea6e6171
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-10-11 17:30:44 +0200

    combinate the tow servies for movies

--- src/watched-movie/service.ts
+++ src/watched-movie/service.ts
@@ -1,10 +1,17 @@
 import {WatchedMovie} from 'eh-domain/model/scrobble/sync';
-import {movieRepository as repo} from './add.repository';
 import {MissingMovieError} from '../error/missing-movie.error';
+import {movieRepository as repo} from './set-watched.repository';
+import {getWatchedMoviesFromDb} from './get-watched.repository';
+import {transformMoviesFromDB} from './transformer';
 
 function setMovieAsWatched(movie: WatchedMovie, userId: number) {
     return findMovieId(movie)
         .then(id => repo.setMovieAsWatched(repo.prepareMovie(movie, id, userId)));
+}
+
+function getWatchedMovies(userId: number): Promise<WatchedMovie[]> {
+    return getWatchedMoviesFromDb(userId)
+            .then(data => transformMoviesFromDB(data));
 }
 
 function findMovieId(movie: WatchedMovie): Promise<number> {
@@ -16,4 +23,4 @@ function findMovieId(movie: WatchedMovie): Promise<number> {
         });
 }
 
-export {setMovieAsWatched};
+export {setMovieAsWatched, getWatchedMovies};
