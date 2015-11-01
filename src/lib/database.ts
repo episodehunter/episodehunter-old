43ab872d6d59cfde79c68be4bbc743d91329e09a
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-01 16:58:21 +0100

    Do not auto complete to the database

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,6 +1,13 @@
 import {config} from '../config';
 import Knex = require('knex');
 
-const knex = Knex(config.db)
+let knex = undefined;
+
+function connect() {
+    if (knex === undefined) {
+        knex = Knex(config.db);
+    }
+    return knex;
+}
 
 export {knex as database};
