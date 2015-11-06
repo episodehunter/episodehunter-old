e751dc0476a7628d1b613ad37621bf3917c7aa88
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-06 23:56:36 +0100

    Add a gulp file to compile both ts and ES6 files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+const babel = require('gulp-babel');
+
+let tsProject = ts.createProject('tsconfig.json', {
+    typescript: require('typescript')
+});
+
+gulp.task('typescript', () =>  {
+    return gulp.src([
+        'typings/tsd.d.ts',
+        'src/**/*.ts'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('build', ['typescript'], () => {
+    return gulp.src('.tmp/**/*.js')
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['build']);
