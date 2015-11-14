8b0c26b31398933506de736a5442499c27440c7a
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-14 16:58:24 +0100

    Add queue message for image ingestor

---
+++ queue/image-ingestor.ts
@@ -0,0 +1,12 @@
+const imageIngestor = {
+    addOrUpdate: {
+        show: {
+            fanart: 'image-ingestor.add-or-update.show.fanart',
+            poster: 'image-ingestor.add-or-update.show.poster',
+            episode: 'image-ingestor.add-or-update.show.episode'
+        }
+    }
+};
+
+export {imageIngestor};
+export default imageIngestor;
