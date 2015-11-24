c47f03d013c8ad9e0ecf04928073d3dd6dbc59c4
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-24 19:48:32 +0100

    CLEAN: Renaming series to show for consistency

--- src/watched-episodes/get-watched.repository.ts
+++ src/watched-episodes/get-watched.repository.ts
@@ -2,7 +2,7 @@ import {show, episode, watchedEpisode} from '../episodehunter-messages/database/
 import {database} from '../lib/database';
 import {as} from '../lib/utility';
 
-interface WatchedSeriesDatabaseInterface {
+interface WatchedShowDatabaseInterface {
     show_id: number;
     show_tvdb_id: number;
     show_imdb_id: string;
@@ -12,25 +12,25 @@ interface WatchedSeriesDatabaseInterface {
     season: number;
 }
 
-function getWatchedEpisodes(userId: number): Promise<WatchedSeriesDatabaseInterface[]> {
+function getWatchedEpisodes(userId: number): Promise<WatchedShowDatabaseInterface[]> {
     let model = {
         episode: episode,
-        series: show,
+        show: show,
         watched: watchedEpisode
     };
 
     return database
         .select(
-            as(model.series.id, 'show_id'),
-            as(model.series.tvdbId, 'show_tvdb_id'),
-            as(model.series.imdbId, 'show_imdb_id'),
-            as(model.series.title, 'show_title'),
-            as(model.series.airs.first, 'show_first_aired'),
+            as(model.show.id, 'show_id'),
+            as(model.show.tvdbId, 'show_tvdb_id'),
+            as(model.show.imdbId, 'show_imdb_id'),
+            as(model.show.title, 'show_title'),
+            as(model.show.airs.first, 'show_first_aired'),
             model.watched.season,
             model.watched.episode
         )
         .from(model.watched.$table)
-        .join(model.series.$table, model.series.id, model.watched.showId)
+        .join(model.show.$table, model.show.id, model.watched.showId)
         .where(model.watched.userId, '=', userId)
         .groupBy(model.watched.showId, model.watched.season, model.watched.episode)
         .then(episodes => {
