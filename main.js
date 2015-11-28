7d422544e244c4caab7948823e9e4e8308a627fa
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-28 22:50:07 +0100

    Add new build

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
 'use strict';
-
-import bunyan from 'bunyan';
-import raven from 'raven';
-import RavenStream from 'bunyan-raven';
-
-let logger;
-
-function createLogger({name, logLevel = 'warn', filePath, stdout, ravenDNS}) {
-    if (!name) {
-        throw new Error('You must specify an application name for the logger');
-    }
-
-    const bunyanConfig = {
-        name,
-        streams: []
-    };
-    let ravenClient;
-
-    if (stdout) {
-        bunyanConfig.streams.push({
-            level: logLevel,
-            stream: process.stdout
-        });
-    }
-
-    if (filePath) {
-        bunyanConfig.streams.push({
-            type: 'rotating-file',
-            level: logLevel,
-            path: filePath,
-            period: '1d',
-            count: 30
-        });
-    }
-
-    if (ravenDNS) {
-        ravenClient = new raven.Client(ravenDNS);
-        bunyanConfig.streams.push({
-            type: 'raw',
-            stream: new RavenStream(ravenClient),
-            level: 'warning'
-        });
-    }
-
-    const bunyanLogger = bunyan.createLogger(bunyanConfig);
-
-    if (logger === undefined) {
-        logger = bunyanLogger;
-    }
-
-    return bunyanLogger;
+var logger;
+exports.logger = logger;
+function createLogger(_a) {
+    var name = _a.name, _b = _a.logLevel, logLevel = _b === void 0 ? 'warn' : _b, filePath = _a.filePath, stdout = _a.stdout, ravenDNS = _a.ravenDNS;
 }
-
-export default logger;
-export {createLogger, logger};
+exports.createLogger = createLogger;
+exports.__esModule = true;
+exports["default"] = logger;
