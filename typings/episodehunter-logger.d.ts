b6862ac93aec51df38d0b59c5a7022887f184ada
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-28 22:50:27 +0100

    Update typings file

--- typings/episodehunter-logger.d.ts
+++ typings/episodehunter-logger.d.ts
@@ -1,21 +1,19 @@
 interface logger {
-    trace(...msg: string[]): void;
-    debug(...msg: string[]): void;
-    info(...msg: string[]): void;
-    warn(...msg: string[]): void;
-    error(...msg: string[]): void;
-    fatal(...msg: string[]): void;
+    trace(msg: string, ...data: any[]): void;
+    debug(msg: string, ...data: any[]): void;
+    info(msg: string, ...data: any[]): void;
+    warn(msg: string, ...data: any[]): void;
+    error(msg: string, ...data: any[]): void;
+    fatal(msg: string, ...data: any[]): void;
 }
 
-interface createLogger {
-    ({name, logLevel, filePath, stdout, ravenDNS}: {
+declare function createLogger({name, logLevel, filePath, stdout, ravenDNS}: {
         name: string;
         logLevel?: string;
         filePath: string;
         stdout: boolean;
         ravenDNS: string;
     }): logger;
-}
 
 declare module 'episodehunter-logger' {
     export default logger;
