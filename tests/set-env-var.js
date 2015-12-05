b82b9e0211e111994ae45c96d513fdccc34b1a71
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-12-05 09:59:58 +0100

    :white_check_mark: Load .env instead of hardcoded envs

--- tests/set-env-var.js
+++ tests/set-env-var.js
@@ -1,5 +1,1 @@
-process.env.EH_EPISODE_IMAGE = 'episode-image-path';
-process.env.EH_SHOW_FANART = 'show-fanart-path';
-process.env.EH_SHOW_POSTER = 'show-poster-path';
-process.env.EH_MOVIE_FANART = 'movie-fanart-path';
-process.env.EH_MOVIE_POSTER = 'movie-poster-path';
+require('dotenv').load();
