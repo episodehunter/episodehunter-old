aacc7cc76857d533b4b13c32e688fcfaf9f7ed89
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-12-28 08:23:44 +0100

    Convert messages to a npm package

---
+++ messages/typings/messages.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../model/ingest/image.d.ts" />
+/// <reference path="../model/scrobble/sync/watched-show.d.ts" />
+/// <reference path="../model/scrobble/sync/watched-movie.d.ts" />
+/// <reference path="../model/ids.d.ts" />
