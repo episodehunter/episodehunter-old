274dc5643301c743dcf052c5ae232d88b27498d3
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-06-13 21:08:03 +0200

    :art: Remove 3part env loader.

--- web-api/src/specs/set-env-var.ts
+++ web-api/src/specs/set-env-var.ts
@@ -1,3 +1,8 @@
-require('dotenv').load();
 process.env.EH_LOG_LEVEL = 'fatal';
 process.env.EH_JWT_SALT = 'asdfghjklpoiy';
+process.env.RAVEN_DNS = '';
+process.env.EH_DB_HOST = '127.0.0.1';
+process.env.EH_DB_USER = 'episodehunter';
+process.env.EH_DB_PASS = 'episodehunter';
+process.env.EH_DB_DB = 'episodehunter';
+process.env.EH_DB_PORT = 33060;
